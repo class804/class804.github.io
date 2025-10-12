547941d0377110ae9369bd1291a86454dfa09319
fafunblocked <mirzajunaid3111@gmail.com>
2025-10-12 12:37:43 +0500

    update

---
+++ g4/cat-gunner-super-zombie/cat-gunner-super-zombie-shoot-poki/remote-cf.js
@@ -0,0 +1,8 @@
+"cordova.plugins.firebase.config.update(3600).then(function() {
+	
+    cordova.plugins.firebase.config.getString(""test"").then(function(value) {
+    
+		return value;
+});
+	
+}).catch((error: any) => return value;);"
---
+++ g4/crazy-cars/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1451138.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': false,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 92059935, 40914487, 41217562, 41261986, 41334679, 41353004, 51407881, 85439481, 85440691, 85448563, 85725639, 86086490, 86476989, 86728818, 86948388, 87240260, 89044191, 89044217, 89044218, 89044220, 89111191, 89613339, 89643901, 89645735, 89660739, 90326092, 90645350, 90892904, 90933266, 91077498, 91233776, 91254353, 91256327, 91258477, 91385248, 91385335, 91387750, 91390444, 91391868, 91400175, 91404918, 91415112, 91415547, 91899381, 91935109, 92090393, 92207486, 92396819, 92574623, 93573824, 94432075, 94952840, 97273109, 97273110, 97273111, 98366456, 100945293, 104225972, 104371667, 104453925, 105165001, 106154795, 107102507, 109133836, 109494582, 136941277, 40857457 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40857457/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40857458/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40857456/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/153453433/1/draco.wasm.js', 'wasmUrl' : 'files/assets/153453434/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/153453432/1/draco.js', 'preload' : true},
+];
